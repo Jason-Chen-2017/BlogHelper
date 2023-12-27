d7a7f39fd9e81ee234a90a00bb954fca5964a07d
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2023-12-27 21:53:53 +0800

    parms['categories'] = '【ALLMA】AI大模型应用实战'  // 专栏: AI实战,大数据AI人工智能,Python实战             parms['original_link'] = ''             parms['resource_url'] = ''             parms['tags'] = '大数据,人工智能,语言模型,AI,LLM,Java,Python,架构设计,Agent,RPA'             // parms['plan_id'] = '3' // 原力计划             parms['creator_activity_id'] = '10645' // 活动 id

--- src/blog/csdn.js
+++ src/blog/csdn.js
@@ -70,12 +70,12 @@ function publishArticleToCSDN(title, markdowncontent, content, isPublish) {
             parms['pubStatus'] = 'draft'
             parms['Description'] = content.toString().substring(0,200)
             parms['authorized_status'] = false
-            parms['categories'] = 'AI实战,大数据AI人工智能,LLM大模型落地实战指南'  // 专栏: AI实战,大数据AI人工智能,Python实战
+            parms['categories'] = '【ALLMA】AI大模型应用实战'  // 专栏: AI实战,大数据AI人工智能,Python实战
             parms['original_link'] = ''
             parms['resource_url'] = ''
             parms['tags'] = '大数据,人工智能,语言模型,AI,LLM,Java,Python,架构设计,Agent,RPA'
             // parms['plan_id'] = '3' // 原力计划
-            // parms['creator_activity_id'] = '10584' // 1024 活动 id
+            parms['creator_activity_id'] = '10645' // 活动 id
 
         }else {
             parms['status'] = 2
