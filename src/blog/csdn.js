6f77ad7f0fe0b8f5d307dcce40c5901408fa4c8a
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2024-01-28 02:20:17 +0800

    parms['categories'] = 'AI大模型应用开发实战案例详解'  // 专栏             parms['original_link'] = ''             parms['resource_url'] = ''             parms['tags'] = '大数据,人工智能,语言模型,AI,LLM,Java,Python,架构设计,Agent,RPA'             // parms['plan_id'] = '3' // 原力计划             parms['creator_activity_id'] = '10657' // 活动 id

--- src/blog/csdn.js
+++ src/blog/csdn.js
@@ -70,12 +70,12 @@ function publishArticleToCSDN(title, markdowncontent, content, isPublish) {
             parms['pubStatus'] = 'draft'
             parms['Description'] = content.toString().substring(0,200)
             parms['authorized_status'] = false
-            parms['categories'] = 'AI大模型应用入门实战与进阶,AI大模型企业级应用开发实战,LLM大模型落地实战指南'  // 专栏: AI实战,大数据AI人工智能,Python实战
+            parms['categories'] = 'AI大模型应用开发实战案例详解'  // 专栏
             parms['original_link'] = ''
             parms['resource_url'] = ''
             parms['tags'] = '大数据,人工智能,语言模型,AI,LLM,Java,Python,架构设计,Agent,RPA'
             // parms['plan_id'] = '3' // 原力计划
-            parms['creator_activity_id'] = '10645' // 活动 id
+            parms['creator_activity_id'] = '10657' // 活动 id
 
         }else {
             parms['status'] = 2
